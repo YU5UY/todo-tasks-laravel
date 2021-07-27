d9787c355fbd2e1cd0ac5b1b7e59b88bd0e030a7
yu5uy <waleedvbn2015@gmail.com>
2021-07-27 17:03:11 +0300

    Set regex validation for username

--- resources/lang/ar/validation.php
+++ resources/lang/ar/validation.php
@@ -97,7 +97,7 @@ return [
     'numeric' => 'The :attribute must be a number.',
     'password' => 'The password is incorrect.',
     'present' => 'The :attribute field must be present.',
-    'regex' => 'The :attribute format is invalid.',
+    'regex' => 'يجب ان يكون اسم المستخدم باللغه الانجليزيه ويبدا بحرف صحيح ولا يحتوي على فراغات او رموز',
     'required' => 'The :attribute field is required.',
     'required_if' => 'The :attribute field is required when :other is :value.',
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
