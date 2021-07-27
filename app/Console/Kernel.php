2248515f4741f81a849b0a7321476f243f30a99e
yu5uy <waleedvbn2015@gmail.com>
2021-07-27 17:03:50 +0300

    Change Time for working task schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('Users:resetTasks')->dailyAt("12:00AM");
+        $schedule->command('Users:resetTasks')->everyMinute();
     }
 
     /**
